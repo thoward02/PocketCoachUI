69e36d6248c7eea6fadbbc91303151aa630d2ab6
thoward02 <36546676+thoward02@users.noreply.github.com>
2019-06-28 11:47:34 -0400

    Added base files

---
+++ index.js
@@ -0,0 +1,24 @@
+//Index
+
+
+function Main(){
+
+
+  //Add hover event
+  var Button = document.getElementById("HomeButton-MainButton-Circle");
+  Button.addEventListener("mouseover", function(){
+
+
+  })
+
+  console.log(Button.style);
+
+}
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  Main();
+
+
+});

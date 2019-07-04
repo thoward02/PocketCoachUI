26462a9aaf6264e0118c0863b123f7e152ddf995
Thoward <36546676+thoward02@users.noreply.github.com>
2019-07-04 12:57:56 -0400

    Finished creating basic button movements

--- index.js
+++ index.js
@@ -2,13 +2,38 @@
 
 
 function Main(){
-
+  //Reset sizing
   document.getElementById("Display").style.width = window.innerWidth;
   document.getElementById("Display").style.height = window.innerHeight;
 
+  //Set Specific values
+  document.getElementById("LeftSide").style.width = "0%";
+  document.getElementById("RightSide").style.width = "100%";
+
+
+  //build main button
+  let Button = document.getElementById("MenuButton");
+
+  //Build event
+  Button.onclick = function(){
+    //Vars
+    let LeftSide = document.getElementById("LeftSide");
+
+    if(LeftSide.style.width != "40%"){
+      document.getElementById("LeftSide").style.width = "40%";
+      document.getElementById("TopBar").style.height = "5%";
+      document.getElementById("RightSide").style.width = "100%";
+
+    }else{
+      document.getElementById("LeftSide").style.width = "0%";
+      document.getElementById("TopBar").style.height = "5%";
+      document.getElementById("RightSide").style.width = "100%";
+
+    }
+
+  }
+
 }
-
-
 
 document.addEventListener("DOMContentLoaded", function(){
 

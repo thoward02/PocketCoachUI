7f7621ea22f5a779be84bd53a683471db5053370
thoward02 <36546676+thoward02@users.noreply.github.com>
2019-07-08 10:05:34 -0400

    Fixed sub menus

--- SubMenu/Guides.js
+++ SubMenu/Guides.js
@@ -1,4 +1,3 @@
-
 class MenuBlock{
   /**
   * Title, Picture and Description
@@ -6,17 +5,29 @@ class MenuBlock{
   **/
 
 
-  constructor(Title){
-    this.Title            = Title;
+  constructor(Title, DataBlock){
+    this.Title                 = Title;
+    this.DescText              = DataBlock.Description;
+    this.ImageSrc              = DataBlock.Image
 
-    this.Elem             = document.createElement("div");
+    //Modify text
+    let NewText = "";
+    for(var x = 0; x < 68; x++){
+      NewText += this.DescText[x];
+    }
+    NewText += " . . .";
 
-    this.Picture          = document.createElement("img");
-    this.Header           = document.createElement("div");
-    this.Description      = document.createElement("div");
+    this.DescText = NewText;
 
-    this.Header.innerHTML = this.Title;
-    this.Picture.src      = "./Resources/OWMaps/Hanamura.jfif"
+    this.Elem                  = document.createElement("div");
+
+    this.Picture               = document.createElement("img");
+    this.Header                = document.createElement("div");
+    this.Description           = document.createElement("div");
+
+    this.Header.innerHTML      = this.Title;
+    this.Description.innerHTML = this.DescText;
+    this.Picture.src           = this.ImageSrc;
 
     //Append
     this.Elem.appendChild(this.Picture);
@@ -60,6 +71,12 @@ class MenuBlock{
       this.Header.style.textAlign   = "left";
       this.Header.style.marginLeft  = "32%";
 
+      //Setup Description
+      this.Description.style.textAlign   = "left";
+      this.Description.style.fontWeight = "200";
+      this.Description.style.fontSize = "30px";
+
+      this.Description.style.marginLeft  = "32%";
 
     }
 }
@@ -67,6 +84,122 @@ class MenuBlock{
 
 //Load Guides
 window.LoadMaps = function(){
+  window.MapTypeData = {
+    "Hybrid" : {
+      "Funct" : window.LoadHybrid,
+      "Class" : null,
+      "SubMaps" : {
+        "Blizzard_World" : {
+          "Funct" : window.LoadMapPage("Blizzard_World"),
+          "Class" : null
+        },
+        "Eichenwalde" : {
+          "Funct" : window.LoadMapPage("Eichenwalde"),
+          "Class" : null
+        },
+        "Hollywood" : {
+          "Funct" : window.LoadMapPage("Hollywood"),
+          "Class" : null
+        },
+        "Kings_Row" : {
+          "Funct" : window.LoadMapPage("Kings_Row"),
+          "Class" : null
+
+        },
+        "Numbani" : {
+          "Funct" : window.LoadMapPage("Numbani"),
+          "Class" : null
+        }
+
+      }
+    },
+    "Payload" : {
+      "Funct" : window.LoadHybrid,
+      "Class" : null,
+      "SubMaps" : {
+        "Blizzard_World" : {
+          "Funct" : window.LoadMapPage("Blizzard_World"),
+          "Class" : null
+        },
+        "Eichenwalde" : {
+          "Funct" : window.LoadMapPage("Eichenwalde"),
+          "Class" : null
+        },
+        "Hollywood" : {
+          "Funct" : window.LoadMapPage("Hollywood"),
+          "Class" : null
+        },
+        "Kings_Row" : {
+          "Funct" : window.LoadMapPage("Kings_Row"),
+          "Class" : null
+
+        },
+        "Numbani" : {
+          "Funct" : window.LoadMapPage("Numbani"),
+          "Class" : null
+        }
+
+      }
+    },
+    "ControlPoint" : {
+      "Funct" : window.LoadHybrid,
+      "Class" : null,
+      "SubMaps" : {
+        "Blizzard_World" : {
+          "Funct" : window.LoadMapPage("Blizzard_World"),
+          "Class" : null
+        },
+        "Eichenwalde" : {
+          "Funct" : window.LoadMapPage("Eichenwalde"),
+          "Class" : null
+        },
+        "Hollywood" : {
+          "Funct" : window.LoadMapPage("Hollywood"),
+          "Class" : null
+        },
+        "Kings_Row" : {
+          "Funct" : window.LoadMapPage("Kings_Row"),
+          "Class" : null
+
+        },
+        "Numbani" : {
+          "Funct" : window.LoadMapPage("Numbani"),
+          "Class" : null
+        }
+
+      }
+    },
+    "CP2" : {
+      "Funct" : window.LoadHybrid,
+      "Class" : null,
+      "SubMaps" : {
+        "Blizzard_World" : {
+          "Funct" : window.LoadMapPage("Blizzard_World"),
+          "Class" : null
+        },
+        "Eichenwalde" : {
+          "Funct" : window.LoadMapPage("Eichenwalde"),
+          "Class" : null
+        },
+        "Hollywood" : {
+          "Funct" : window.LoadMapPage("Hollywood"),
+          "Class" : null
+        },
+        "Kings_Row" : {
+          "Funct" : window.LoadMapPage("Kings_Row"),
+          "Class" : null
+
+        },
+        "Numbani" : {
+          "Funct" : window.LoadMapPage("Numbani"),
+          "Class" : null
+        }
+
+      }
+    },
+
+
+  }
 
   //Reset title
   document.getElementById("Title").innerHTML = "Maps";
@@ -81,8 +214,10 @@ window.LoadMaps = function(){
   let MapData = window.Data.Guides.Maps.MapTypes;
 
   for(var items in MapData) {
-    let Block = new MenuBlock(items);
+    let Block = new MenuBlock(items, MapData[items]);
+    Block.Elem.onclick = MapTypeData[items].Funct;
     document.getElementById("DataHolder").appendChild(Block.Elem);
+
   }
 
 
@@ -98,7 +233,7 @@ window.LoadHeroes = function(){
   let HeroData = window.Data.Guides.Heroes.HeroTypes;
 
   for(var items in HeroData){
-    let Block = new MenuBlock(items);
+    let Block = new MenuBlock(items, HeroData[items]);
     document.getElementById("DataHolder").appendChild(Block.Elem);
   }
 }
@@ -112,7 +247,102 @@ window.LoadTeamComps = function(){
   let TeamCompData = window.Data.Guides.TeamComps.Comps;
 
   for(var items in TeamCompData){
-    let Block = new MenuBlock(items);
-    document.getElementById("DataHolder").appendChild(Block.Elem);
+    let Block = new MenuBlock(items, TeamCompData[items]);
+    document.getElementById("DataHolder").appendChild(Block.Elem);;
   }
 }
+
+
+
+
+
+
+
+//MAP TYPE LOADS
+
+window.LoadHybrid   = function(){
+  document.getElementById("Title").innerHTML = "Hybrid";
+  document.getElementById("DataHolder").innerHTML = "";
+
+  let HybridData = window.Data.Guides.Maps.MapTypes.Hybrid.Maps;
+
+  for(var items in HybridData) {
+    let Block = new MenuBlock(items, HybridData[items]);;
+    Block.Elem.addEventListener("click", function(){
+
+      
+
+    })
+
+    document.getElementById("DataHolder").appendChild(Block.Elem);
+
+  }
+
+}
+window.LoadPayload  = function(){
+
+  document.getElementById("Title").innerHTML = "Payload";
+  document.getElementById("DataHolder").innerHTML = "";
+  document.getElementById("AppHolder-BlackBox").onclick();
+
+  let PayloadData = window.Data.Guides.Maps.MapTypes.Payload.Maps;
+
+  for(var items in PayloadData) {
+    let Block = new MenuBlock(items, PayloadData[items]);
+    Block.Elem.onclick = PayloadData[items].Funct;
+    document.getElementById("DataHolder").appendChild(Block.Elem);
+
+  }
+}
+
+window.LoadCP       = function(){
+
+  document.getElementById("Title").innerHTML = "CP";
+  document.getElementById("DataHolder").innerHTML = "";
+  document.getElementById("AppHolder-BlackBox").onclick();
+
+  let CPData = window.Data.Guides.Maps.MapTypes.CP.Maps;
+
+  for(var items in CPData) {
+    let Block = new MenuBlock(items, CPData[items]);
+    Block.Elem.onclick = CPData[items].Funct;
+    document.getElementById("DataHolder").appendChild(Block.Elem);
+
+  }
+
+}
+window.Load2CP      = function(){
+
+  document.getElementById("Title").innerHTML = "2CP";
+  document.getElementById("DataHolder").innerHTML = "";
+  document.getElementById("AppHolder-BlackBox").onclick();
+
+  let CP2Data = window.Data.Guides.Maps.MapTypes.CP2.Maps;
+
+  for(var items in CP2Data) {
+    let Block = new MenuBlock(items, CP2Data[items]);
+    Block.Elem.onclick = window.MapTypeData[items].Funct;
+    document.getElementById("DataHolder").appendChild(Block.Elem);
+
+  }
+
+}
+
+window.LoadMapPage  = function(Map){
+    console.log(Map);
+  //BUILD PAGE BASED OFF OF MAP PARAM
+  // MAP -> maps.js -> maps.Guides.Maps.MapList.Maps[Map]
+  //Will return the guide, we'll build the guide here
+}
+
+
+//HERO TYPE LOAD
+
+
+
+
+
+//TEAM COMP TYPE LOAD
+
+
+//i'm glad i wrote everything in 3 menus, it's like i planned this ovo
--- maps.js
+++ maps.js
@@ -1,3 +1,5 @@
+//https://d1u1mce87gyfbn.cloudfront.net/hero/genji/hero-select-portrait.png
+
 window.Data = {
 
     "Guides" : {
@@ -6,19 +8,35 @@ window.Data = {
         "MapTypes" : {
           "Hybrid"       : {
             "Description" : "Hybrid is a hybrid game type between control point (CP) and payload. You must first capture the control point, then push the payload to the end of the map.",
-
+            "Image" : "https://liquipedia.net/commons/images/thumb/b/b0/Kings_row_map.jpg/600px-Kings_row_map.jpg",
             "Maps"   : {
-              "Blizzard_World" : "/Api/Guides/Maps/Blizzard_World",
-              "Eichenwalde" : "/Api/Guides/Maps/Eichenwalde",
-              "Hollywood" : "/Api/Guides/Maps/Hollywood",
-              "Kings_Row" : "/Api/Guides/Maps/Kings_Row",
-              "Numbani" : "/Api/Guides/Maps/Numbani"
+              "Blizzard_World" : {
+                "Description" : "Hybrid is a hybrid game type between control point (CP) and payload. You must first capture the control point, then push the payload to the end of the map.",
+                "Image" : "https://liquipedia.net/commons/images/thumb/b/b0/Kings_row_map.jpg/600px-Kings_row_map.jpg",
+              },
+              "Eichenwalde" : {
+                "Description" : "Hybrid is a hybrid game type between control point (CP) and payload. You must first capture the control point, then push the payload to the end of the map.",
+                "Image" : "https://liquipedia.net/commons/images/thumb/b/b0/Kings_row_map.jpg/600px-Kings_row_map.jpg",
+              },
+              "Hollywood" : {
+                "Description" : "Hybrid is a hybrid game type between control point (CP) and payload. You must first capture the control point, then push the payload to the end of the map.",
+                "Image" : "https://liquipedia.net/commons/images/thumb/b/b0/Kings_row_map.jpg/600px-Kings_row_map.jpg",
+              },
+              "Kings_Row" : {
+                "Description" : "Hybrid is a hybrid game type between control point (CP) and payload. You must first capture the control point, then push the payload to the end of the map.",
+                "Image" : "https://liquipedia.net/commons/images/thumb/b/b0/Kings_row_map.jpg/600px-Kings_row_map.jpg",
+
+              },
+              "Numbani" : {
+                "Description" : "Hybrid is a hybrid game type between control point (CP) and payload. You must first capture the control point, then push the payload to the end of the map.",
+                "Image" : "https://liquipedia.net/commons/images/thumb/b/b0/Kings_row_map.jpg/600px-Kings_row_map.jpg",
+              }
 
             }
           },
           "Payload"      : {
             "Description" : "Payload is all about pushing the cart from one end of the map to the other, by pushing it to three points. Players must either push the payload or stop other players from doing the same.",
-
+            "Image" : "https://www.owfire.com/images/maps/route-66-1.jpg",
             "Maps"   : {
               "Dorado" : "/Api/Guides/Maps/Dorado",
               "Havana" : "/Api/Guides/Maps/Havana",
@@ -31,6 +49,7 @@ window.Data = {
           },
           "ControlPoint" : {
             "Description" : "Control point is akin to King of the Hill, where two teams battle to control the center point. First team to win 2 out of the three maps wins the game.",
+            "Image" : "https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/f/f3/Nepal_loading_screen.jpg/1200px-Nepal_loading_screen.jpg",
             "Maps" : {
               "Busan" : "/Api/Guides/Maps/Busan",
               "Ilios" : "/Api/Guides/Maps/Ilios",
@@ -40,8 +59,9 @@ window.Data = {
             }
 
           },
-          "2CP"          : {
+          "CP2"          : {
             "Description" : "2 CP is where teams must defend or attack two control points. If the attacking team manages to take the first point, the defending team must rotate back to the second point and defend.",
+            "Image" : "https://steamuserimages-a.akamaihd.net/ugc/92729171400080787/7B699835AA5D4FADBA98A644A8B6FFB66B37CF3E/",
             "Maps" : {
               "Horizon_Lunar_Colony" : "/Api/Guides/Maps/Horizon_Lunar_Colony",
               "Paris" : "/Api/Guides/Maps/Paris",
@@ -53,26 +73,226 @@ window.Data = {
         },
         "MapList" : {
           "Maps" :  {
-            "Blizzard_World" : "/Api/Guides/Maps/Blizzard_World",
-            "Eichenwalde" : "/Api/Guides/Maps/Eichenwalde",
-            "Hollywood" : "/Api/Guides/Maps/Hollywood",
-            "Kings_Row" : "/Api/Guides/Maps/Kings_Row",
-            "Numbani" : "/Api/Guides/Maps/Numbani",
-            "Dorado" : "/Api/Guides/Maps/Dorado",
-            "Havana" : "/Api/Guides/Maps/Havana",
-            "Junkertown" : "/Api/Guides/Maps/Junkertown",
-            "Rialto" : "/Api/Guides/Maps/Rialto",
-            "Route_66" : "/Api/Guides/Maps/Route_66",
-            "Watchpoint_Gibraltar" : "/Api/Guides/Maps/Watchpoint_Gibraltar",
-            "Horizon_Lunar_Colony" : "/Api/Guides/Maps/Horizon_Lunar_Colony",
-            "Paris" : "/Api/Guides/Maps/Paris",
-            "Temple_of_Anubis" : "/Api/Guides/Maps/Temple_of_Anubis",
-            "Volskaya_Industries" : "/Api/Guides/Maps/Volskaya_Industries",
-            "Busan" : "/Api/Guides/Maps/Busan",
-            "Ilios" : "/Api/Guides/Maps/Ilios",
-            "Lijiang_Tower" : "/Api/Guides/Maps/Lijiang_Tower",
-            "Nepal" : "/Api/Guides/Maps/Nepal",
-            "Oasis" : "/Api/Guides/Maps/Oasis"
+            "Blizzard_World" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Eichenwalde" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Hollywood" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Kings_Row" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Numbani" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Dorado" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Havana" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Junkertown" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Rialto" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Route_66" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Watchpoint_Gibraltar" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Horizon_Lunar_Colony" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Paris" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Temple_of_Anubis" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Volskaya_Industries" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Busan" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Ilios" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Lijiang_Tower" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Nepal" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            },
+            "Oasis" : {
+              "Description" : "",
+              "Image" : "",
+              "GuideData" : {
+                "Header" : "",
+                "body"   : {
+                  "Header" : "",
+                  "Text"   : ""
+                }
+              }
+            }
           }
         }
 
@@ -80,20 +300,52 @@ window.Data = {
 
       "Heroes"    : {
         "HeroTypes" : {
-          "Dps"         : {},
-          "MainSupport" : {},
-          "OffSupport"  : {} ,
-          "MainTank"    : {},
-          "OffTank"     : {}
+          "Dps"          : {
+            "Description" : "These Heroes are the primary damage dealing heroes within Overwatch",
+            "Image" : "https://d1u1mce87gyfbn.cloudfront.net/hero/genji/icon-portrait.png",
+          },
+          "MainSupport" : {
+            "Description" : "Main supports are your healers in charge of keeping the entire team alive",
+            "Image" : "https://d1u1mce87gyfbn.cloudfront.net/hero/ana/icon-portrait.png",
+          },
+          "OffSupport"  : {
+            "Description" : "Offsupports exist to keep the main healer alive, and provide utility to the team",
+            "Image" : "https://d1u1mce87gyfbn.cloudfront.net/hero/lucio/icon-portrait.png",
+          } ,
+          "MainTank"    : {
+            "Description" : "Main tanks take space for the team and provide a front line for the team to stand behind",
+            "Image" : "https://d1u1mce87gyfbn.cloudfront.net/hero/reinhardt/icon-portrait.png",
+          },
+          "OffTank"     : {
+            "Description" : "Off tanks keep the team alive through peels, and provides support to the Main Tank in taking space",
+            "Image" : "https://d1u1mce87gyfbn.cloudfront.net/hero/dva/icon-portrait.png",
+          }
         },
-        "HeroList"  : {}
+        "HeroList"  : {
+
+
+
+        }
       },
 
       "TeamComps" : {
         "Comps"     : {
-          "222"       : {},
-          "3-3"       : {},
-          "OffMeta"   : {}
+          "2-2-2"       : {
+            "Description" : "Two two two comps are team comps that hold 2 tanks, 2 dps and 2 healers. It's the standard comp type ran in games all over.",
+            "Image" : "",
+            "GuideData" : {}
+          },
+          "3-3"       : {
+            "Description" : "Three Three is a comp type involving 3 tanks and 3 healers, with the most prominant form being known as GOATs.",
+            "Image" : "",
+            "GuideData" : {}
+
+          },
+          "OffMeta"   : {
+            "Description" : "This category is for what could be consider as off meta, or cheese comps",
+            "Image" : "",
+            "GuideData" : {}
+          }
         }
       }
 
@@ -101,22 +353,33 @@ window.Data = {
 
     "Quizzes" : {
       "Easy"   : {
+        "Description" : "These are the easier quizzes that lower ranked players should be able to get right",
+        "Image" : "",
         "Content" : {}
       },
       "Medium" : {
+        "Description" : "These are medium ranked quizzes that mid tier players should be able to get right",
+        "Image" : "",
         "Content" : {}
       },
       "Hard"   : {
+        "Description" : "These are harder quzzies that high level players should be able to get right",
+        "Image" : "",
         "Content" : {}
       }
     },
 
     "Coaching" : {
       "Requests" : {
-
+        "Description" : "",
+        "Image" : "",
+        "Data" : {}
       },
 
       "Vods" : {
+        "Description" : "",
+        "Image" : "",
+        "Data" : {}
 
       }
 
@@ -125,10 +388,14 @@ window.Data = {
     "OWLVods" : {
 
       "RawVod" : {
-
+        "Description" : "",
+        "Image" : "",
+        "Data" : {}
       },
       "CommentatedVod" : {
-
+        "Description" : "",
+        "Image" : "",
+        "Data" : {}
       }
     }
 
